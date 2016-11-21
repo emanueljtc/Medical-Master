296117c5560b5e542744a89320439fa8d36fe849
Emanuel Torres <emanueljtc@gmail.com>
2016-11-20 20:35:54 -0400

    pagescontroller

--- app/Model/Person.php
+++ app/Model/Person.php
@@ -124,7 +124,7 @@ class Person extends AppModel {
 			'numeric' => array(
 				'rule' => array('numeric'),
 				//'message' => 'Your custom message here',
-				'allowEmpty' => false,
+				'allowEmpty' =>  true,
 				'required' => false,
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations

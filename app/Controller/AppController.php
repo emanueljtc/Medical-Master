544192992e54159a6ca76792ddf1261dee617a1f
Emanuel Torres <emanueljtc@gmail.com>
2016-11-30 18:13:08 -0400

    CakeAdminLTE

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -33,6 +33,7 @@ App::uses('Controller', 'Controller');
 class AppController extends Controller {
 
     public $components = array('RequestHandler');
-    
+
      public $uses = array('Post');
+     public $theme = "CakeAdminLTE";
 }

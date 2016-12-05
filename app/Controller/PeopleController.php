800800628bc86b7666c05307fb92c81295f87568
Emanuel Torres <emanueljtc@gmail.com>
2016-12-05 19:41:51 -0400

    Cambios Persona Controller

--- app/Controller/PeopleController.php
+++ app/Controller/PeopleController.php
@@ -1,26 +1,5 @@
 <?php
 App::uses('AppController', 'Controller');
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-	if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-
-            Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-                
-        exit(0);
-        
-    }
 /**
  * People Controller
  *
@@ -35,7 +14,7 @@ class PeopleController extends AppController {
  *
  * @var array
  */
-	public $components = array('Paginator', 'Session','RequestHandler');
+	public $components = array('Paginator', 'Session');
 
 /**
  * index method
@@ -44,11 +23,8 @@ class PeopleController extends AppController {
  */
 	public function index() {
 		$this->Person->recursive = 0;
-		$this->set('people', $this->Paginator->paginate());
-		$this->set('personas',$this->Person->find('all'));
-   		$this->set('_serialize', array('personas'));
+		$this->set('people', $this->paginate());
 	}
-
 
 /**
  * view method
@@ -64,13 +40,7 @@ class PeopleController extends AppController {
 		$options = array('conditions' => array('Person.' . $this->Person->primaryKey => $id));
 		$this->set('person', $this->Person->find('first', $options));
 	}
-	public function api($id){
-				$persona = $this->Person->findById($id);
-				$this->set(array(
-				'persona' => $persona,
-				'_serialize' => array('persona')
-						));
-					}
+
 /**
  * add method
  *
@@ -80,16 +50,14 @@ class PeopleController extends AppController {
 		if ($this->request->is('post')) {
 			$this->Person->create();
 			if ($this->Person->save($this->request->data)) {
-				$this->Session->setFlash(__('The person has been saved.'));
-				return $this->redirect(array('action' => 'index'));
+				$this->Session->setFlash(__('The person has been saved'), 'flash/success');
+				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The person could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The person could not be saved. Please, try again.'), 'flash/error');
 			}
 		}
-		$rols = $this->Person->Rol->find('list');
 		$nationalities = $this->Person->Nationality->find('list');
-		$specialties = $this->Person->Specialty->find('list');
-		$this->set(compact('rols', 'nationalities', 'specialties'));
+		$this->set(compact('nationalities'));
 	}
 
 /**
@@ -100,44 +68,46 @@ class PeopleController extends AppController {
  * @return void
  */
 	public function edit($id = null) {
+        $this->Person->id = $id;
 		if (!$this->Person->exists($id)) {
 			throw new NotFoundException(__('Invalid person'));
 		}
-		if ($this->request->is(array('post', 'put'))) {
+		if ($this->request->is('post') || $this->request->is('put')) {
 			if ($this->Person->save($this->request->data)) {
-				$this->Session->setFlash(__('The person has been saved.'));
-				return $this->redirect(array('action' => 'index'));
+				$this->Session->setFlash(__('The person has been saved'), 'flash/success');
+				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The person could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The person could not be saved. Please, try again.'), 'flash/error');
 			}
 		} else {
 			$options = array('conditions' => array('Person.' . $this->Person->primaryKey => $id));
 			$this->request->data = $this->Person->find('first', $options);
 		}
-		$rols = $this->Person->Rol->find('list');
 		$nationalities = $this->Person->Nationality->find('list');
-		$specialties = $this->Person->Specialty->find('list');
-		$this->set(compact('rols', 'nationalities', 'specialties'));
+		$this->set(compact('nationalities'));
 	}
 
 /**
  * delete method
  *
  * @throws NotFoundException
+ * @throws MethodNotAllowedException
  * @param string $id
  * @return void
  */
 	public function delete($id = null) {
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
 		$this->Person->id = $id;
 		if (!$this->Person->exists()) {
 			throw new NotFoundException(__('Invalid person'));
 		}
-		$this->request->allowMethod('post', 'delete');
 		if ($this->Person->delete()) {
-			$this->Session->setFlash(__('The person has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The person could not be deleted. Please, try again.'));
+			$this->Session->setFlash(__('Person deleted'), 'flash/success');
+			$this->redirect(array('action' => 'index'));
 		}
-		return $this->redirect(array('action' => 'index'));
+		$this->Session->setFlash(__('Person was not deleted'), 'flash/error');
+		$this->redirect(array('action' => 'index'));
 	}
 }

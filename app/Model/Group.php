c726d2712f3148faa4f922a26d895d34f1859040
Emanuel Torres <emanueljtc@gmail.com>
2016-12-07 18:04:32 -0400

    Grupo _ Model _ Update

--- app/Model/Group.php
+++ app/Model/Group.php
@@ -23,7 +23,7 @@ class Group extends AppModel {
 		'name' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
+				'message' => 'Campo Vacio',
 				//'allowEmpty' => false,
 				//'required' => false,
 				//'last' => false, // Stop validation after this rule
